918f786e34fc126e3463f8dd33c5e941659ce371
graypawn <choi.pawn@gmail.com>
2016-04-09 13:14:06 +0900

    Error Handling

--- wrenet/__main__.py
+++ wrenet/__main__.py
@@ -17,23 +17,31 @@
 
 import argparse
 import wrenet
+import Registry
 from wrenet.wrenet import get_interfaces
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path', help='Windows system registry file')
-    parser.add_argument('--version', action='version',
-                        version='%(prog)s ' + wrenet.__version__)
-    parser.add_argument('-r', '--root', action='store_true',
-                        help='Use the Windows mount point as path')
-    args = parser.parse_args()
 
+parser = argparse.ArgumentParser()
+parser.add_argument('path', help='Windows system registry file')
+parser.add_argument('--version', action='version',
+                    version='%(prog)s ' + wrenet.__version__)
+parser.add_argument('-r', '--root', action='store_true',
+                    help='Use the Windows mount point as path')
+args = parser.parse_args()
+
+
+def main():
     if args.root == True:
         args.path = args.path + "/Windows/System32/config/SYSTEM"
-
-    for interface in get_interfaces(args.path):
-        interface.print_all()
-        print()
+    try:
+        interfaces = get_interfaces(args.path)
+        for interface in interfaces:
+            interface.print_all()
+            print()
+    except Registry.RegistryParse.ParseException as e:
+        print("%s: %s: Invalid registry file" % (parser.prog, args.path))
+    except Exception as e:
+        print("%s: %s: %s" % (parser.prog, args.path, e.args[1]))
 
 
 if __name__ == "__main__":
